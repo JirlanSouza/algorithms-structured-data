edf28d3bc1e3de010423a3e6ff55e3136c865c82
Jirlan Souza <jirlansouza08@gmail.com>
2023-01-08 19:02:57 -0400

    feat: created the TestBase class to manager tests assert and print result

--- algorithms/sort/test/SortAlgorithmsTest.java
+++ algorithms/sort/test/SortAlgorithmsTest.java
@@ -20,6 +20,17 @@ public class SortAlgorithmsTest {
                     21, 21, 21,
                     296, 296, });
 
+    private static TestLists expectedLists = new TestLists(
+            new int[] { 26, 42, 53, 225, 283, 396, 438, 447, 451, 484, 513, 523, 575, 738, 808, 830, 839, 977, 995,
+                    998 },
+            new int[] { 26, 42, 53, 225, 283, 396, 438, 447, 451, 484, 513, 523, 575, 738, 808, 830,
+                    839, 977, 995,
+                    998, },
+            new int[] { 26, 42, 53, 225, 283, 396, 438, 447, 451, 484, 513, 523, 575, 738, 808, 830, 839, 977, 995,
+                    998 },
+            new int[] { 21, 21, 21, 48, 48, 48, 56, 56, 56, 78, 78, 102, 102, 102, 296, 296, 296, 459, 459, 896, 896,
+                    896 });
+
     public static void main(String[] args) {
         quickSortTest();
         bublleSortTest();
@@ -28,119 +39,60 @@ public class SortAlgorithmsTest {
     }
 
     private static void quickSortTest() {
-        printNewTest("QUICK SORT TEST");
-        printList("Any list", Color.BLUE, testLists.any());
         int[] sortedAnyList = QuickSort.sort(testLists.any().clone());
-        printList("Sorted Any list", Color.GREEN, sortedAnyList);
+        TestBase.test("QUICK SORT TEST ANY LIST", expectedLists.any(), sortedAnyList);
 
-        printList("Already sorted list", Color.BLUE, testLists.alreadySorted());
         int[] sortedAlreadySortedList = QuickSort.sort(testLists.alreadySorted().clone());
-        printList("Sorted Alreaddy list", Color.GREEN, sortedAlreadySortedList);
+        TestBase.test("QUICK SORT TEST ALREADY SORTED LIST", expectedLists.alreadySorted(), sortedAlreadySortedList);
 
-        printList("inverse list", Color.BLUE, testLists.inversedSorted());
         int[] sortedInverseList = QuickSort.sort(testLists.inversedSorted().clone());
-        printList("Sorted Inversy list", Color.GREEN, sortedInverseList);
+        TestBase.test("QUICK SORT TEST INVERSE SORTED LIST", expectedLists.inversedSorted(), sortedInverseList);
 
-        printList("Repeated list", Color.BLUE, testLists.repeadted());
         int[] sortedRepeatedList = QuickSort.sort(testLists.repeadted().clone());
-        printList("Sorted Inversy list", Color.GREEN, sortedRepeatedList);
+        TestBase.test("QUICK SORT TEST REPEATED LIST", expectedLists.repeadted(), sortedRepeatedList);
+
     }
 
     private static void bublleSortTest() {
-        printNewTest("BUBBLE SORT TEST");
-        printList("Any list", Color.BLUE, testLists.any());
         int[] sortedAnyList = BubbleSort.sort(testLists.any().clone(), false);
-        printList("Sorted Any list", Color.GREEN, sortedAnyList);
+        TestBase.test("BUBBLE SORT TEST ANY LIST", expectedLists.any(), sortedAnyList);
 
-        printList("Already sorted list", Color.BLUE, testLists.alreadySorted());
         int[] sortedAlreadySortedList = BubbleSort.sort(testLists.alreadySorted().clone(), false);
-        printList("Sorted Alreaddy list", Color.GREEN, sortedAlreadySortedList);
+        TestBase.test("BUBBLE SORT TEST ALREADY SORTED LIST", expectedLists.alreadySorted(), sortedAlreadySortedList);
 
-        printList("inverse list", Color.BLUE, testLists.inversedSorted());
         int[] sortedInverseList = BubbleSort.sort(testLists.inversedSorted().clone(), false);
-        printList("Sorted Inversy list", Color.GREEN, sortedInverseList);
+        TestBase.test("BUBBLE SORT TEST INVERSE SORTED LIST", expectedLists.inversedSorted(), sortedInverseList);
 
-        printList("Repeated list", Color.BLUE, testLists.repeadted());
         int[] sortedRepeatedList = BubbleSort.sort(testLists.repeadted().clone(), false);
-        printList("Sorted Inversy list", Color.GREEN, sortedRepeatedList);
+        TestBase.test("BUBBLE SORT TEST REPEATED LIST", expectedLists.repeadted(), sortedRepeatedList);
     }
 
     private static void mergeSortTest() {
-        printNewTest("MERGE SORT TEST");
-        printList("Any list", Color.BLUE, testLists.any());
         int[] sortedAnyList = MergeSort.sort(testLists.any().clone());
-        printList("Sorted Any list", Color.GREEN, sortedAnyList);
+        TestBase.test("MERGE SORT TEST ANY LIST", expectedLists.any(), sortedAnyList);
 
-        printList("Already sorted list", Color.BLUE, testLists.alreadySorted());
         int[] sortedAlreadySortedList = MergeSort.sort(testLists.alreadySorted().clone());
-        printList("Sorted Alreaddy list", Color.GREEN, sortedAlreadySortedList);
+        TestBase.test("MERGE SORT TEST ALREADY SORTED LIST", expectedLists.alreadySorted(), sortedAlreadySortedList);
 
-        printList("inverse list", Color.BLUE, testLists.inversedSorted());
         int[] sortedInverseList = MergeSort.sort(testLists.inversedSorted().clone());
-        printList("Sorted Inversy list", Color.GREEN, sortedInverseList);
+        TestBase.test("MERGE SORT TEST INVERSE SORTED LIST", expectedLists.inversedSorted(), sortedInverseList);
 
-        printList("Repeated list", Color.BLUE, testLists.repeadted());
         int[] sortedRepeatedList = MergeSort.sort(testLists.repeadted().clone());
-        printList("Sorted Inversy list", Color.GREEN, sortedRepeatedList);
+        TestBase.test("MERGE SORT TEST REPEATED LIST", expectedLists.repeadted(), sortedRepeatedList);
     }
 
     private static void heapSortTest() {
-        printNewTest("HEAP SORT TEST");
-        printList("Any list", Color.BLUE, testLists.any());
         int[] sortedAnyList = new BinaryHeap(testLists.any().clone()).sort();
-        printList("Sorted Any list", Color.GREEN, sortedAnyList);
+        TestBase.test("HEAP SORT TEST ANY LIST", expectedLists.any(), sortedAnyList);
 
-        printList("Already sorted list", Color.BLUE, testLists.alreadySorted());
         int[] sortedAlreadySortedList = new BinaryHeap(testLists.alreadySorted().clone()).sort();
-        printList("Sorted Alreaddy list", Color.GREEN, sortedAlreadySortedList);
+        TestBase.test("HEAP SORT TEST ALREADY SORTED LIST", expectedLists.alreadySorted(), sortedAlreadySortedList);
 
-        printList("inverse list", Color.BLUE, testLists.inversedSorted());
         int[] sortedInverseList = new BinaryHeap(testLists.inversedSorted().clone()).sort();
-        printList("Sorted Inversy list", Color.GREEN, sortedInverseList);
+        TestBase.test("HEAP SORT TEST INVERSE SORTED LIST", expectedLists.inversedSorted(), sortedInverseList);
 
-        printList("Repeated list", Color.BLUE, testLists.repeadted());
         int[] sortedRepeatedList = new BinaryHeap(testLists.repeadted().clone()).sort();
-        printList("Sorted Inversy list", Color.GREEN, sortedRepeatedList);
+        TestBase.test("HEAP SORT TEST REPEATED LIST", expectedLists.repeadted(), sortedRepeatedList);
     }
 
-    private static void printList(String prefix, Color color, int[] list) {
-        StringBuilder formetedPrefix = new StringBuilder();
-        formetedPrefix.append(String.format("\u001b[%d;1m %-20s\u001b[0m ", color.getValue(), prefix));
-
-        for (int listItem : list) {
-            formetedPrefix.append(listItem + ", ");
-        }
-
-        formetedPrefix.delete(formetedPrefix.length() - 2, formetedPrefix.length() - 1);
-        formetedPrefix.append("\n");
-        System.out.println(formetedPrefix);
-    }
-
-    private static void printNewTest(String name) {
-        String testHeader = String.format("\u001b[%d;1m %-150s\u001b[0m \n", Color.BLUE.getValue(), name);
-        System.out.println(testHeader);
-    }
-
-}
-
-final record TestLists(
-        int[] any,
-        int[] alreadySorted,
-        int[] inversedSorted,
-        int[] repeadted) {
-}
-
-enum Color {
-    GREEN(42),
-    BLUE(44);
-
-    private int value;
-
-    Color(int value) {
-        this.value = value;
-    }
-
-    public int getValue() {
-        return this.value;
-    }
 }
