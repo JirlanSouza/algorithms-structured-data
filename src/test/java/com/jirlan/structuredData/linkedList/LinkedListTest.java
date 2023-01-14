abce7b974e8f6385f22ca58a486e7002956e1c30
Jirlan Souza <jirlansouza08@gmail.com>
2023-01-14 18:39:29 -0400

    test: add the LinkedList tests

--- src/test/java/com/jirlan/structuredData/linkedList/LinkedListTest.java
+++ src/test/java/com/jirlan/structuredData/linkedList/LinkedListTest.java
@@ -25,4 +25,30 @@ public class LinkedListTest {
 
         assertEquals(list.size(), 2);
     }
+
+    @Test
+    public void prependNodeToLinkedList() {
+        var list = new LinkedList<Integer>();
+
+        list.prepend(2);
+        list.append(1);
+        list.prepend(3);
+
+        assertEquals(list.toString(), " 3, 2, 1,");
+    }
+
+    @Test
+    public void insertNodeToLinkedList() {
+        var list = new LinkedList<Integer>();
+
+        list.insert(4, 3);
+        list.insert(3, 2);
+        list.insert(2, 1);
+        list.insert(1, -7);
+        list.insert(10, 9);
+
+        System.out.println(list);
+
+        assertEquals(list.toString(), " 1, 4, 2, 3, 10,");
+    }
 }
