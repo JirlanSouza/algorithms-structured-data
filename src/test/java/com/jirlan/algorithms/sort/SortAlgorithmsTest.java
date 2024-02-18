1e63c56115aa3f5cb475b8e4f5f75f7c546354bc
Jirlan Souza <jirlansouza08@gmail.com>
2024-02-18 19:51:02 -0400

    algorithms: refactor `SortAlgorithmsTest` add @DisplayName annotation

--- src/test/java/com/jirlan/algorithms/sort/SortAlgorithmsTest.java
+++ src/test/java/com/jirlan/algorithms/sort/SortAlgorithmsTest.java
@@ -1,6 +1,7 @@
 package com.jirlan.algorithms.sort;
 
 import com.jirlan.common.ExecutionTime;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
@@ -16,42 +17,38 @@ public class SortAlgorithmsTest {
 
     @ParameterizedTest
     @MethodSource("testsData")
-    public void bubbleSortTest(SortAlgorithmsDataTests.DataTest dataTest) {
-        System.out.println("Bubble sort");
+    @DisplayName("Should sort array integer number using Bubble sort")
+    void bubbleSortTest(SortAlgorithmsDataTests.DataTest dataTest) {
         var sortResult = ExecutionTime.mettle(dataTest.given().clone(), BubbleSort::sort);
 
         assertArrayEquals(dataTest.expected(), sortResult);
-        System.out.println();
     }
 
     @ParameterizedTest
     @MethodSource("testsData")
-    public void mergeSortTest(SortAlgorithmsDataTests.DataTest dataTest) {
-        System.out.println("Merge sort");
+    @DisplayName("Should sort array integer number using Merge sort")
+    void mergeSortTest(SortAlgorithmsDataTests.DataTest dataTest) {
         var sortResult = ExecutionTime.mettle(dataTest.given().clone(), BubbleSort::sort);
 
         assertArrayEquals(dataTest.expected(), sortResult);
-        System.out.println();
     }
 
     @ParameterizedTest
     @MethodSource("testsData")
-    public void quickSortTest(SortAlgorithmsDataTests.DataTest dataTest) {
-        System.out.println("Quick sort");
+    @DisplayName("Should sort array integer number using Quick sort")
+    void quickSortTest(SortAlgorithmsDataTests.DataTest dataTest) {
         var sortResult = ExecutionTime.mettle(dataTest.given().clone(), BubbleSort::sort);
 
         assertArrayEquals(dataTest.expected(), sortResult);
-        System.out.println();
     }
 
     @ParameterizedTest
     @MethodSource("testsData")
-    public void heapSortTest(SortAlgorithmsDataTests.DataTest dataTest) {
-        System.out.println("Heap sort");
+    @DisplayName("Should sort array integer number using Heap sort")
+    void heapSortTest(SortAlgorithmsDataTests.DataTest dataTest) {
         var sortResult = ExecutionTime.mettle(dataTest.given().clone(), BubbleSort::sort);
 
         assertArrayEquals(dataTest.expected(), sortResult);
-        System.out.println();
     }
 
 }
